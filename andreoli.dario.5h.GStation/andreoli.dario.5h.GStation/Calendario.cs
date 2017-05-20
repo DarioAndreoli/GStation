3bc67a15744036b4f655864c737561d80246d44c
DarioAndreoli <dada-rn@hotmail.it>
2017-05-20 11:15:16 +0200

    Add files via upload

---
+++ andreoli.dario.5h.GStation/andreoli.dario.5h.GStation/Calendario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace andreoli.dario._5h.GStation
+{
+    class Calendario
+    {
+        public string Evento { get; set; }
+        public string Data { get; set; }
+    }
+}
